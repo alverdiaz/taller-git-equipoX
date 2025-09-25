9268a52976a2118036a8049803c3f3b5e3d72492
camargo <camargoo1028@gmail.com>
2025-09-24 20:01:02 -0500

    Agrega operación de multiplicación y división

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
 if __name__ == "__main__":
     print("Suma:", suma(5, 3))
     print("Resta:", resta(5, 3))
+    print("Multiplicación:", multiplicacion(5, 3))
+    print("División:", division(5, 3))
+
+    
